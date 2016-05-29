df29463819cdf87104665911187d35003897e28f
Abhi120695 <Abhishekupadhyay153@gmail.com>
2016-05-29 16:08:02 +0530

    added grid adapter

--- app/src/main/java/com/rentflees/rentflees/UI/Explordetail.java
+++ app/src/main/java/com/rentflees/rentflees/UI/Explordetail.java
@@ -10,6 +10,7 @@ public class Explordetail extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_explordetail);
+        setContentView(R.layout.appartmentdetailcard);
+
     }
 }
